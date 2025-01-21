f66ebb4483c2aa3e15c74059ae33a491314fc1ca
Gaurav Kumar <gauravkumar@Gauravs-MacBook.local>
2025-01-22 00:04:49 +0530

    Added Routing Layer setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import express from "express";
 import { StatusCodes } from "http-status-codes";
 
+import connectDB from "./config/dbConfig.js";
 import { PORT } from "./config/serverConfig.js";
+import apiRouter from './routes/apiRouter.js';
 
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api',apiRouter)
 app.get("/ping", (req, res) => {
   res.status(StatusCodes.OK).json({ message: "Hi Server Gaurav!" });
 });
 
 app.listen(PORT, () => {
   console.log("listening on port", PORT);
+  connectDB();
 });
